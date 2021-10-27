50d4ffb449944a5c2c05461fba2111a7c0fad1f4
Jiehuang Shi <jiehuangs@student.unimelb.edu.au>
2021-10-27 18:36:25 +1100

    now can search user by both username and email

--- app/src/main/java/com/example/comp90018_project/Activity/FindNewFriendActivity.java
+++ app/src/main/java/com/example/comp90018_project/Activity/FindNewFriendActivity.java
@@ -51,7 +51,6 @@ public class FindNewFriendActivity extends AppCompatActivity {
         if (currentUser == null){
             reload();
         }else {
-            // TODO: 2021/10/23 Add construction of layout
             searchEvent();
         }
     }
@@ -71,58 +70,17 @@ public class FindNewFriendActivity extends AppCompatActivity {
 
     public void finduser(){
         content = findViewById(R.id.search_bar);
-        String username = content.getText().toString();
-        if (!username.isEmpty()){
+        String search_content = content.getText().toString();
+        if (!search_content.isEmpty()){
             CollectionReference userRef = mDB.collection("users");
-            Query query = userRef.whereEqualTo("username", username);
-            query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                    ListView FriendListview = (ListView) findViewById(R.id.searchList);
-                    try {
-                        if (task.getResult().getDocuments().get(0).getData() == null) {
-                            //If this username isn't stored in database
-                            Log.i(TAG, "We cannot find this user");
-                            Toast.makeText(FindNewFriendActivity.this, "We cannot find this user", Toast.LENGTH_LONG).show();
-                        } else {
-                            //Get the corresponding email address
-                            Log.i(TAG, String.valueOf(task.getResult().getDocuments().get(0).getData()));
-                            userfound = new User(task.getResult().getDocuments().get(0).getData());
-                            Log.i(TAG, "Find user");
-                        }
-                        if (userfound != null) {
-                            List<Map<String, Object>> userfound_list = new ArrayList<Map<String, Object>>();
-                            Map<String, Object> map = new HashMap<String, Object>();
-                            if (userfound.getAvatarUrl() == null) {
-                                map.put("avatar", R.drawable.a);
-                            }
-                            else {
-                                map.put("avatar", userfound.getAvatarUrl());
-                            }
-                            map.put("name", userfound.getUsername());
-                            userfound_list.add(map);
-                            FriendAdapter adapter = new FriendAdapter(FindNewFriendActivity.this);
-                            adapter.setFriendList(userfound_list);
-                            FriendListview.setAdapter(adapter);
-                            FriendListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                                @Override
-                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                    String selectUser = userfound.getUid();
-                                    Log.d(TAG, "selected:" + selectUser);
-                                    Intent intent = new Intent(FindNewFriendActivity.this, ProfileActivity.class);
-                                    intent.putExtra(EXTRA_MESSAGE, selectUser);
-                                    startActivity(intent);
-                                }
-                            });
-                        }
-                    }
-                    catch (java.lang.IndexOutOfBoundsException e) {
-                        Log.i(TAG, "User is not exist");
-                        Toast.makeText(FindNewFriendActivity.this, "User is not exist", Toast.LENGTH_LONG).show();
-                        FriendListview.setAdapter(null);
-                    }
-                }
-            });
+            if (search_content.contains("@")) {
+                Query emailQuery = userRef.whereEqualTo("email", search_content);
+                getQuery(emailQuery);
+            }
+            else {
+                Query query = userRef.whereEqualTo("username", search_content);
+                getQuery(query);
+            }
         }
         else {
             ListView FriendListview = (ListView) findViewById(R.id.searchList);
@@ -130,6 +88,57 @@ public class FindNewFriendActivity extends AppCompatActivity {
             FriendListview.setAdapter(null);
         }
 
+    }
+
+    private void getQuery(Query query) {
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                ListView FriendListview = (ListView) findViewById(R.id.searchList);
+                try {
+                    if (task.getResult().getDocuments().get(0).getData() == null) {
+                        //If this username isn't stored in database
+                        Log.i(TAG, "We cannot find this user");
+                        Toast.makeText(FindNewFriendActivity.this, "We cannot find this user", Toast.LENGTH_LONG).show();
+                    } else {
+                        //Get the corresponding email address
+                        Log.i(TAG, String.valueOf(task.getResult().getDocuments().get(0).getData()));
+                        userfound = new User(task.getResult().getDocuments().get(0).getData());
+                        Log.i(TAG, "Find user");
+                    }
+                    if (userfound != null) {
+                        List<Map<String, Object>> userfound_list = new ArrayList<Map<String, Object>>();
+                        Map<String, Object> map = new HashMap<String, Object>();
+                        if (userfound.getAvatarUrl() == null) {
+                            map.put("avatar", R.drawable.a);
+                        }
+                        else {
+                            map.put("avatar", userfound.getAvatarUrl());
+                        }
+                        map.put("name", userfound.getUsername());
+                        userfound_list.add(map);
+                        FriendAdapter adapter = new FriendAdapter(FindNewFriendActivity.this);
+                        adapter.setFriendList(userfound_list);
+                        FriendListview.setAdapter(adapter);
+                        FriendListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                String selectUser = userfound.getUid();
+                                Log.d(TAG, "selected:" + selectUser);
+                                Intent intent = new Intent(FindNewFriendActivity.this, ProfileActivity.class);
+                                intent.putExtra(EXTRA_MESSAGE, selectUser);
+                                startActivity(intent);
+                            }
+                        });
+                    }
+                }
+                catch (java.lang.IndexOutOfBoundsException e) {
+                    Log.i(TAG, "User is not exist");
+                    Toast.makeText(FindNewFriendActivity.this, "User is not exist", Toast.LENGTH_LONG).show();
+                    FriendListview.setAdapter(null);
+                }
+            }
+        });
     }
 
     //if user does not log in, return to the Login
